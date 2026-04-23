aed0606a6995b112188706e1931e98a2d9b99cf5
olamide828 <adegboyega00001@gmail.com>
2026-04-23 09:05:15 -0700

    redeploy slug fixes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -45,11 +45,6 @@ class ProductController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        //
-    }
-
     /**
      * Store a newly created resource in storage.
      */
