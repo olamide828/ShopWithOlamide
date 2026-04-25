3136673750497d3a8a7245d59921bf729653accb
olamide828 <adegboyega00001@gmail.com>
2026-04-25 14:45:43 -0700

    changed dollar sign to naira

--- resources/js/pages/products.tsx
+++ resources/js/pages/products.tsx
@@ -468,7 +468,7 @@ const Products = () => {
                                         value={formData.price}
                                         onChange={handleChange}
                                         type="number"
-                                        placeholder="Price ($)"
+                                        placeholder="Price (₦)"
                                         required
                                         className="w-full rounded-lg border border-gray-200 px-3 py-2.5 text-sm outline-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-100"
                                     />
